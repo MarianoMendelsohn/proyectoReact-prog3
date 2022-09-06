9a9a7199ca299bc9a2cebb21943e3a3503711cc0
MarianoMendelsohn <mmendelsohn@udesa.edu.ar>
2022-09-06 16:10:59 -0300

    ...

--- proyecto-integrador-1/src/App.js
+++ proyecto-integrador-1/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Header from './components/Header/Header';
 import Footer from './components/Footer/Footer';
+import Home from './components/Home/Home'
 
 
 function App() {
@@ -8,11 +9,14 @@ function App() {
   const opciones = ['Home', 'Favoritos']
 
   return(
-    <>
+    
+    <div>
     <Header menu={opciones}/>
+    <Home/>
     <Footer/>
-    </>
-)
+    </div>
+    
+  )
 }
 
 export default App;
---
+++ proyecto-integrador-1/src/components/Home/Home.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import './styles.css'; 
+
+function Home(props) {
+    <form action="" method="GET" ClassName="search-form">
+					<input type="text" name="search" placeholder="Buscar productos, marcas y más" ClassName="search-form_input"></input>
+					<button type="submit" ClassName="search-form_button"><i ClassName="fas fa-search"></i></button>
+	</form>;
+
+    <div>
+        <section>
+            <h2>TopHub</h2>
+        </section>
+    </div>;
+    <div>
+    <section>
+        <h2>PainHub</h2>
+    </section>
+</div>
+}
+
+export default Home
